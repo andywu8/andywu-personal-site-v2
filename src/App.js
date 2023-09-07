8322b3dfddc3e25790813bc9780ca13ea99339a2
Andy Wu <andy.wu@yale.edu>
2023-09-06 23:34:30 -0700

    add in basic info

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <h1>About Me</h1>
           <h2>Hi I'm Andy Wu!</h2>
           <img class="Headshot hvr-float" src={require('./image/grad_headshot.jpeg')} alt="Headshot"></img>
+          <p>I'm a new grad from Yale class of 2023. I graduated with a BS double majoring in Computer Science & Economics and Statistics & Data Science. I'm interested in software engineering, product management, and data science.</p>
           <ul>
             <li>Passionate Reader</li>
             <li>Lover of all things sports: F1, Basketball, Esports</li>
@@ -39,10 +40,18 @@ function App() {
         <section id="experience">
           <h1>Experience</h1>
           <Internship class="hvr-float" title="Software Engineer Intern" company="Meta" img_src="/meta_logo.jpeg"/>
+          <Internship class="hvr-float" title="Venture Associate" company="Tsai CITY" img_src="/meta_logo.jpeg"/>
+          <Internship class="hvr-float" title="Software Engineer Intern" company="State Farm" img_src="/meta_logo.jpeg"/>
+          <Internship class="hvr-float" title="Machine Learning Research Intern" company="Yale Intelligent Computing Lab" img_src="/meta_logo.jpeg"/>
+
         </section>
         <section id="projects">
           <h1>Projects</h1>
-          <Project class="hvr-float" project_name="Mutuals" description="Match Making App that allows you to match friends of friends"/>
+          <Project class="hvr-float" dates="2022" project_name="Mutuals" description="Match Making App that allows you to match friends of friends"/>
+          <Project class="hvr-float" dates="2022" project_name="Curbside" description="Match Making App that allows you to match friends of friends"/>
+          <Project class="hvr-float" dates="2022" project_name="Mutuals" description="Match Making App that allows you to match friends of friends"/>
+          <Project class="hvr-float" dates="2022" project_name="Simmer" description="Match Making App that allows you to match friends of friends"/>
+
         </section>
 
       </body>
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -6,6 +6,7 @@ export default function Project(props) {
     return (
     <div class={"Card "+props.class}>
         <h1>{props.project_name}</h1>
+        <p>Dates worked on: {props.date}</p>
         <p>{props.description}</p>
         <p>Key Skills: {props.key_skills}</p>
         <a href="/" class="btn">Learn More</a>
