4fbace2fe71aec43fbee21501dc4b298e33d1ba9
Andy Wu <andyww09@gmail.com>
2023-10-22 18:12:40 -0700

    change picture to left of about

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,91 +4,101 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import CardMedia from '@mui/material/CardMedia';
+import Grid from '@material-ui/core/Grid';
 
 
 export default function About(props) {
     return (
-        <Box
-        direction="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-        >
-            <Typography variant="h4" align="center" color="text.primary" paragraph>
-                Hi I'm Andy!
-            </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={4}>
+          <Box
+            display='flex'
+            flexDirection= "column"
+            justifyContent='center'
+            alignItems='center'
+          >
+          <Typography variant="h4" align="center" color="text.primary" paragraph>
+            Hi I'm Andy!
+          </Typography>
 
-            
-            <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            >
-              <CardMedia
-                sx={{ height: 300, width: 300, boxShadow: 3,borderRadius: 2,}}
-                image='/images/grad_headshot.jpeg'
-              />
-            </Box>
+          </Box>
+          
 
-            <br></br>
-            <Box fullWidth variant='outlined' 
-            sx={{
-                bgcolor: '#f9f9f9',
-                boxShadow: 3,
-                borderRadius: 2,
-                p: 4,
-                minWidth: 300,
-            }}>
-            <Typography variant="h6" align="center" color="text.primary" paragraph>
-                I'm a new grad double majoring in CS & Econ and S&DS from Yale class of 2023 looking for new grad SWE roles. I have experience in data science, ML, and full stack.
-            </Typography>
-            <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            >
+          <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          >
+            <CardMedia
+              sx={{ height: 300, width: 300, boxShadow: 3,borderRadius: 2,}}
+              image='/images/grad_headshot.jpeg'
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6} md={8}>
+          <Box>
+              <br></br>
+              <Box fullWidth variant='outlined' 
+              sx={{
+                  bgcolor: '#f9f9f9',
+                  boxShadow: 3,
+                  borderRadius: 2,
+                  p: 4,
+                  minWidth: 300,
+              }}>
+              <Typography variant="h6" align="center" color="text.primary" paragraph>
+                  I'm a new grad double majoring in CS & Econ and S&DS from Yale class of 2023 looking for new grad SWE roles. I have experience in data science, ML, and full stack.
+              </Typography>
+              <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              >
+                <ul>
+                  <li>
+                    <Link href="https://cpsc.yale.edu/" underline="hover">
+                        B.S. in Computer Science & Economomics
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="https://statistics.yale.edu/" underline="hover">
+                        B.A. in Statistics & Data Science
+                    </Link>
+                  </li>
+                </ul>
+              </Box>
+              <Typography variant="h6" align="center" color="text.primary" paragraph>
+                  I love to read books, especially fantasy novels. Some of my favorite series are Harry Potter, Percy Jackson, and The Mistborn Series.
+                  I love sports: F1, Basketball, and Esports. 
+                  I love animals and am the proud owner of a bunny.
+                  I'm currently based in San Francisco, but am open to relocation.
+              </Typography>
+              <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                  Socials
+              </Typography>
+              <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              >
               <ul>
                 <li>
-                  <Link href="https://cpsc.yale.edu/" underline="hover">
-                      B.S. in Computer Science & Economomics
-                  </Link>
+                  <a href="https://www.linkedin.com/in/andywu-yale/">
+                      <LinkedInIcon style={{ color: "#0A66C2", fontSize: '50px'}}/>
+                  </a>
                 </li>
                 <li>
-                  <Link href="https://statistics.yale.edu/" underline="hover">
-                      B.A. in Statistics & Data Science
-                  </Link>
+                  <a href="https://www.linkedin.com/in/andywu-yale/">
+                      <GitHubIcon style={{ color: "black",  fontSize: '50px'}}/>
+                  </a>
                 </li>
               </ul>
-            </Box>
-            <Typography variant="h6" align="center" color="text.primary" paragraph>
-                I love to read books, especially fantasy novels. Some of my favorite series are Harry Potter, Percy Jackson, and The Mistborn Series.
-                I love sports: F1, Basketball, and Esports. 
-                I love animals and am the proud owner of a bunny.
-                I'm currently based in San Francisco, but am open to relocation.
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                Socials
-            </Typography>
-            <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            >
-            <ul>
-              <li>
-                <a href="https://www.linkedin.com/in/andywu-yale/">
-                    <LinkedInIcon style={{ color: "#0A66C2", fontSize: '50px'}}/>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/andywu-yale/">
-                    <GitHubIcon style={{ color: "black",  fontSize: '50px'}}/>
-                </a>
-              </li>
-            </ul>
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </Grid>
+        
+      </Grid>
     )
 }
 
